44c3684d6d35aeda63d654611ffa45fa0b08df2a
youxiudeyapi <1342544475@qq.com>
2018-11-07 18:30:38 +0800

    十一月七号

---
+++ yi.php
@@ -0,0 +1,39 @@
+<?php 
+echo ugly(14);
+function ugly($ugly)
+{
+    if($ugly==1)return 1;
+    if(getUgly($ugly)==false)return "不是丑数";
+    $j = 1;
+    for($i=2;;$i++)
+    {
+        $a = getUgly($i);
+        if($a){
+            $j++;
+        }
+        if($i==$ugly){
+            return $j;
+        }
+    }
+}
+function getUgly($ugly)
+{
+   if($ugly<=0) return false;
+   while($ugly!=1)
+   {
+       while($ugly%2==0)
+       {
+           $ugly = $ugly/2;
+       }
+       while($ugly%3==0){
+           $ugly = $ugly/3;
+       }
+       while($ugly%5==0){
+           $ugly = $ugly/5;
+       }
+       return $ugly==1?true:false;
+   }
+}
+
+
+ ?>
