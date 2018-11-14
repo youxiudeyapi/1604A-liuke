c710026f9961eb80a57aac061f2685ec2d11c0c1
youxiudeyapi <1342544475@qq.com>
2018-11-14 19:10:13 +0800

    11.14

---
+++ l.php
@@ -0,0 +1,20 @@
+<?php
+//echo upn(5);
+//function upn($n)
+//{
+//    $arr = range(1,$n);
+//    $array = array_sum($arr);
+//    return $array;
+//}
+
+//echo num(20,30);
+//function num($num1,$num2){
+//    $a=array_sum($num1,$num2);
+//    return $a;
+//}
+
+$a = 1.21354;
+$b = 2.123133;
+$c=bcmul($a,$b,6);
+var_dump($c);
+?>
