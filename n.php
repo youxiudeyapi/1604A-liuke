ad02fbd42e67909d6e3ef2e2904a54dadcddc8e5
youxiudeyapi <1342544475@qq.com>
2018-11-15 18:29:39 +0800

    11.15

---
+++ n.php
@@ -0,0 +1,32 @@
+<?php 
+function upAndS($array,$s)
+{
+    $a = end($array);
+    $b = prev($array);
+    if(($a+$b)<$s) return false;
+    foreach($array as $key=>$val){
+        if($val>$s){
+            unset($array[$key]);
+        }
+    }
+    $arr = array();
+    foreach($array as $key=>$val){
+        foreach($array as $k=>$v){
+            if($val!=$v){
+                $a = $val+$v;
+                if($a==$s){
+                    $arr[$key] = array($val,$v);
+                }
+            }
+        }
+        unset($array[$key]);
+    }
+    $are = array();
+    foreach($arr as $key=>$val){
+        $are[] = array_product($val);
+    }
+    sort($are);
+    return $are[0];
+}
+
+ ?>
